64648fb7f700803da9c11a427d9d5c49858c79d4
Tajemniczy <SFBreakTF2@gmail.com>
2022-04-21 01:09:32 +0200

    Dumb fix

--- SpectatorList/EventHandlers.cs
+++ SpectatorList/EventHandlers.cs
@@ -29,7 +29,7 @@ namespace SpectatorList
                 StringBuilder list = StringBuilderPool.Shared.Rent();
                 int count = 0;
 
-                if (player.CurrentSpectatingPlayers.Count() != 1)
+                if (player.CurrentSpectatingPlayers.Count() == 1)
                 {
                     list.Append("<align=right><size=45%><color=orange><b>Spectators ({COUNT}):</b>");
                     foreach (Player splayer in player.CurrentSpectatingPlayers)
