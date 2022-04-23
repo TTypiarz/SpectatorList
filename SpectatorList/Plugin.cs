b9b084773472982a3104a9a3a767336984872f2c
Tajemniczy <SFBreakTF2@gmail.com>
2022-04-23 18:05:08 +0200

    Version update

--- SpectatorList/Plugin.cs
+++ SpectatorList/Plugin.cs
@@ -17,7 +17,7 @@ namespace SpectatorList
 
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 1, 1);
 
         public override void OnEnabled()
         {
