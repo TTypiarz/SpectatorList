ecf2f42953b774b1fe598d3754992f1b4916477a
Tajemniczy <SFBreakTF2@gmail.com>
2022-05-06 22:46:08 +0200

    yoink

--- SpectatorList/Plugin.cs
+++ SpectatorList/Plugin.cs
@@ -17,7 +17,7 @@ namespace SpectatorList
 
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
-        public override Version Version { get; } = new Version(1, 1, 1);
+        public override Version Version { get; } = new Version(1, 1, 2);
 
         public override void OnEnabled()
         {
@@ -32,10 +32,10 @@ namespace SpectatorList
         public override void OnDisabled()
         {
             Singleton = null;
-            EventHandlers = null;
 
             Player.Verified -= EventHandlers.OnVerified;
 
+            EventHandlers = null;
             base.OnDisabled();
         }
     }
