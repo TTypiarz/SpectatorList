25244867b76466ebb6848b75d29e866937547c18
Tajemniczy <SFBreakTF2@gmail.com>
2022-05-09 13:30:47 +0200

    Version bump

--- SpectatorList/Plugin.cs
+++ SpectatorList/Plugin.cs
@@ -16,7 +16,7 @@ namespace SpectatorList
 
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
-        public override Version Version { get; } = new Version(1, 1, 2);
+        public override Version Version { get; } = new Version(1, 1, 3);
 
         public override void OnEnabled()
         {
