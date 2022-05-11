e03958efca2013ab28b5fd489648c23412a3f904
Tajemniczy <SFBreakTF2@gmail.com>
2022-05-12 01:11:12 +0200

    NW API Pain

--- SpectatorList/Config.cs
+++ SpectatorList/Config.cs
@@ -1,9 +1,9 @@
-﻿using Exiled.API.Interfaces;
+﻿using PluginAPI.Core.Interfaces;
 using System.ComponentModel;
 
 namespace SpectatorList
 {
-    public sealed class Config : IConfig
+    public sealed class PluginConfig : IPluginConfig
     {
         [Description("Whether or not the plugin is enabled on this server.")]
         public bool IsEnabled { get; set; } = true;
@@ -13,5 +13,13 @@ namespace SpectatorList
 
         [Description("Whether or not Northwood Staff should be Ignored. (Global Moderators will ALWAYS be Ignored)")]
         public bool IgnoreNorthwood { get; set; } = false;
+
+        [Description("Set the Spectator list Title - Use (COUNT) to get number of Spectators, Use (COLOR) to get current Role color.")]
+        public string Title { get; set; } = "<align=right><size=45%><color=(COLOR)><b>👥 Spectators ((COUNT)):</b></color></size></align>";
+
+        [Description("How names should be displayed - Use (NAME) to get player name, Type (none) if you don't want to show their names.")]
+        public string Names { get; set; } = "<align=right><size=45%><color=(COLOR)>\n(NAME)</color></size></align>";
+
+        public bool ShowDebugMessages { get; set; } = false;
     }
 }
--- SpectatorList/Plugin.cs
+++ SpectatorList/Plugin.cs
@@ -1,37 +1,68 @@
-﻿using Exiled.API.Features;
+﻿using MEC;
+using NorthwoodLib.Pools;
+using PluginAPI.Core;
+using PluginAPI.Events.EventArgs;
 using System;
-using Player = Exiled.Events.Handlers.Player;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SpectatorList
 {
-    public class Plugin : Plugin<Config, Translation>
+    public class Plugin : Plugin<PluginConfig>
     {
-        private EventHandlers EventHandlers;
-
-        public override string Author { get; } = "TTypiarz";
-
         public override string Name { get; } = "SpectatorList";
-
-        public override string Prefix { get; } = "SpectatorList";
-
-        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
-
+        public override string Author { get; } = "TTypiarz";
         public override Version Version { get; } = new Version(1, 1, 3);
 
         public override void OnEnabled()
         {
-            EventHandlers = new EventHandlers(this);
-            Player.Verified += EventHandlers.OnVerified;
+            Player.Joined += Player_Joined;
 
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            Player.Verified -= EventHandlers.OnVerified;
-            EventHandlers = null;
+            Player.Joined -= Player_Joined;
 
             base.OnDisabled();
+        }
+
+        private void Player_Joined(PlayerJoinEvent ev) =>
+    Timing.RunCoroutine(SpectatorList(ev.Player).CancelWith(ev.Player.GameObject));
+
+        private IEnumerator<float> SpectatorList(Player player)
+        {
+            while (true)
+            {
+                yield return Timing.WaitForSeconds(1);
+                yield return Timing.WaitUntilTrue(() => player.IsAlive);
+
+                StringBuilder list = StringBuilderPool.Shared.Rent();
+                list.Append(Config.Title);
+
+                int count = 0;
+
+                foreach (ReferenceHub refhub in player.ReferenceHub.spectatorManager.ServerCurrentSpectatingPlayers)
+                {
+                    Player splayer = Player.Get(refhub.gameObject);
+
+                    if (splayer == player || splayer.IsGlobalModerator ||
+                        splayer.Overwatch && Config.IgnoreOverwatch ||
+                        splayer.IsNorthwoodStaff && Config.IgnoreNorthwood) continue;
+
+                    if (!Config.Names.Equals("(none)", StringComparison.Ordinal))
+                        list.Append(Config.Names.Replace("(NAME)", splayer.Nickname));
+
+                    count++;
+                }
+
+                if (count > 0)
+                    player.ShowHint(
+                        StringBuilderPool.Shared.ToStringReturn(list).Replace("(COUNT)", $"{count}")
+                            .Replace("(COLOR)", $"{CharacterClassManager._staticClasses.Get(player.Role).classColor.ToHex()}"), 1.2f);
+                else StringBuilderPool.Shared.Return(list);
+            }
         }
     }
 }
