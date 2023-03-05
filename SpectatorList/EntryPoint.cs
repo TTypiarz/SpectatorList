08fcd7aa63260284474bd7168797fe50eaa599ec
Jesus-QC <jesusquirocampos@gmail.com>
2023-03-05 22:58:35 +0100

    SpectatorList recoded

---
+++ SpectatorList/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+using SpectatorList.Components;
+using SpectatorList.EventHandlers;
+
+namespace SpectatorList;
+
+public class EntryPoint
+{
+    public static EntryPoint Instance { get; private set; }
+
+    public Harmony Harmony = new Harmony("spectatorlist.taj.com");
+
+    [PluginConfig] 
+    public SpectatorListConfig SpectatorListConfig;
+
+    [PluginEntryPoint("SpectatorList", "1.1.5", "This Plugin allows you to see who is currently Spectating you and watching your every move.", "TTypiarz and Jesus-QC")]
+    public void OnLoad()
+    {
+        Instance = this;
+        EventManager.RegisterEvents<PlayerEventsHandler>(this);
+        
+        SpectatorListController.RefreshRate = SpectatorListConfig.RefreshRate;
+        
+        Harmony.PatchAll();
+    }
+}
