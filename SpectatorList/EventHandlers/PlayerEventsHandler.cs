5ba72080251d7ba3ff96172bb6ceffabf3afa8e0
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2023-03-06 01:57:29 +0100

    Update PlayerEventsHandler.cs

--- SpectatorList/EventHandlers/PlayerEventsHandler.cs
+++ SpectatorList/EventHandlers/PlayerEventsHandler.cs
@@ -18,6 +18,12 @@ public class PlayerEventsHandler
     {
         Player player = Player.Get(display.netIdentity);
 
+        if (!player.IsAlive)
+        {
+            player.Connection.Send(new HintMessage(hint));
+            return;
+        }
+
         if (!SpectatorListController.Controllers.ContainsKey(player) || hint is not TextHint textHint)
             return;
 
